7d932ba1928608aeb41cb743c9d509c583ee8583
Anthony Sottile <asottile@umich.edu>
2020-09-13 19:23:53 -0700

    initial commit

---
+++ make_description.py
@@ -0,0 +1,26 @@
+import argparse
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('times_file')
+    args = parser.parse_args()
+
+    with open(args.times_file) as f:
+        lines = [line.strip() for line in f]
+
+    ts = 0
+    for line in lines:
+        ch, cm, cs = ts // (60 * 60), ts % (60 * 60) // 60, ts % 60
+        print(f'- {ch:02}:{cm:02}:{cs:02} -')
+        p1, _, p2 = line.split()
+        p1_h, p1_m, p1_s = p1.split(':')
+        p2_h, p2_m, p2_s = p2.split(':')
+        p1_ts = 60 * 60 * int(p1_h) + 60 * int(p1_m) + int(p1_s)
+        p2_ts = 60 * 60 * int(p2_h) + 60 * int(p2_m) + int(p2_s)
+        ts += p2_ts - p1_ts
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
---
+++ split.py
@@ -0,0 +1,39 @@
+import argparse
+import subprocess
+import time
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('times_file')
+    parser.add_argument('video_file')
+    args = parser.parse_args()
+
+    with open(args.times_file) as f:
+        lines = [line.strip() for line in f]
+
+    for i, line in enumerate(lines, 1):
+        print(f'doing {i}/{len(lines)}', flush=True)
+        p1, _, p2 = line.split()
+        t0 = time.time()
+        subprocess.check_call(
+            (
+                'ffmpeg',
+                '-hwaccel', 'cuda', '-hwaccel_output_format', 'cuda',
+                '-ss', p1,
+                '-to', p2,
+                '-i', args.video_file,
+                '-preset', 'slow',
+                '-c:v', 'hevc_nvenc',
+                f'{i:02}_clip.mov',
+            ),
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+        print(f'cut from {p1} to {p2} in {time.time()-t0:.2f}s', flush=True)
+
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
